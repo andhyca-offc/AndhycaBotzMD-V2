c4ac2193fa84d111684582a9a59ba545bec7083d
Ary Botz <108046927+aryjuanda@users.noreply.github.com>
2022-07-06 00:04:43 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* Dani Offcial
+*ɴᴀᴍᴇ:* Ary Offcial
 *ᴀɢᴇ:* 15ᵗʰ
 *sᴛᴀᴛᴜs:* Hanya Butuh Pasangan:v 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 16 Desember 2007
-*ᴀᴅᴅʀᴇss:* Indonesia, Kalsel, Kandangan
+*ʙɪʀᴛʜᴅᴀʏ:* 16 Desember 2006
+*ᴀᴅᴅʀᴇss:* Indonesia, Sumut, Sibolangit
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
